f692339e37a077b6720a0cbddd4d2ba28c795c65
brntsw <bruno.pardini6@gmail.com>
2016-10-12 22:39:31 -0300

    - Added the Butterknife lib - Added the LoginUtils to validate the login fields

--- app/src/main/java/bruno/udacity/com/studentguardian/ui/activity/LoginActivity.java
+++ app/src/main/java/bruno/udacity/com/studentguardian/ui/activity/LoginActivity.java
@@ -1,33 +1,61 @@
 package bruno.udacity.com.studentguardian.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import bruno.udacity.com.studentguardian.R;
+import bruno.udacity.com.studentguardian.utils.LoginUtils;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @BindView(R.id.coordinator_login)
+    CoordinatorLayout coordinatorLogin;
+
     @BindView(R.id.toolbar)
-    protected Toolbar toolbar;
+    Toolbar toolbar;
+
+    @BindView(R.id.edit_email)
+    EditText editEmail;
+
+    @BindView(R.id.edit_password)
+    EditText editPassword;
+
+    @BindView(R.id.bt_login)
+    Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
-    }
-
-    protected void onResume(){
-        super.onResume();
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
 
         toolbar.setTitle(getString(R.string.title_activity_login));
+    }
+
+    public void onResume(){
+        super.onResume();
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginUtils utils = new LoginUtils(coordinatorLogin);
+
+                if(utils.validateLoginFields(editEmail, editPassword)){
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/bruno/udacity/com/studentguardian/utils/LoginUtils.java
@@ -0,0 +1,43 @@
+package bruno.udacity.com.studentguardian.utils;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.util.Patterns;
+import android.widget.EditText;
+
+import bruno.udacity.com.studentguardian.R;
+
+/**
+ * Created by Bruno on 12/10/2016.
+ */
+public class LoginUtils {
+
+    private CoordinatorLayout layout;
+
+    public LoginUtils(CoordinatorLayout layout){
+        this.layout = layout;
+    }
+
+    public static boolean isFieldEmpty(EditText editText){
+        return editText.toString().equals("");
+    }
+
+    public boolean validateLoginFields(EditText editEmail, EditText editPassword){
+        if(editEmail.getText().toString().equals("")){
+            Snackbar.make(layout, R.string.type_email, Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(editEmail.getText().toString()).matches()){
+            Snackbar.make(layout, R.string.provide_valid_email, Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(editPassword.getText().toString().equals("")){
+            Snackbar.make(layout, R.string.type_password, Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+}
